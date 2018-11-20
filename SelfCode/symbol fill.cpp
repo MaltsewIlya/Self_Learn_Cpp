03b2cea09afe35c5884a93f036236496d35dffef
MaltsewIlya <45176750+MaltsewIlya@users.noreply.github.com>
2018-11-20 09:04:14 +0200

    Update symbol fill.cpp

--- SelfCode/symbol fill.cpp
+++ SelfCode/symbol fill.cpp
@@ -1,29 +1,25 @@
 #include<iostream>
-#include<string>
 
-
-int main ()
+int main()
 {
-  char symbol;
-  int height;
-  int weight;
-  std::string line;
-  int index_H = 0;
-  int index_W = 0;
-  
-  std::cout << "Choise symbol... ";
-  std::cin >> symbol;
-  std::cout << "Height... ";
-  std::cin >> height;
-  std::cout << "Weight... ";
-  std::cin >> weight;
-  std:: << std::endl;
-  while (index_W < weight) {
-    line += symbol;
-    index_W++;
-  }
-  
-  while (index_H < height) {
-    std::cout << line << std::endl;
-    index_H++;
-  }
+	int height, weight;
+	char symbol;
+
+	std::cout << "Choise symbol... ";
+	std::cin >> symbol;
+	std::cout << "Height... ";
+	std::cin >> height;
+	std::cout << "Weight... ";
+	std::cin >> weight;
+	std::cout << std::endl;
+
+	for (int h = 0; h < height; h++)
+	{
+		for (int w = 0; w < weight; w++)
+		{
+			std::cout << symbol;
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+}
